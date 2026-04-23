a0d09590e5ee1f595c6b0288bc6cf7c978fd8eb0
Gibson Thompson <gibsonthompson1@gmail.com>
2026-04-23 19:29:42 -0400

    Fix time clock import path

--- app/time/page.js
+++ app/time/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef, useCallback } from 'react'
-import { useTimeAuth } from '../layout'
+import { useTimeAuth } from './layout'
 
 export default function TimeClockPage() {
   const { user } = useTimeAuth()
